bc2ad94565afcdcc8f25231c9fe1de236e510f60
Igor Khripunov <igor.khripunov@veeam.com>
2023-06-30 20:45:42 +0400

    Add total price

--- src/presenter/header-presenter.js
+++ src/presenter/header-presenter.js
@@ -8,22 +8,35 @@ export default class HeaderPresenter {
   #container = null;
   #tripsModel = null;
   #filters = null;
+  #tripPoints = null;
 
   constructor({headerContainer, tripsModel}) {
     this.#container = headerContainer;
     this.#tripsModel = tripsModel;
     this.#filters = generateFilter(this.#tripsModel.points);
+    this.tripPoints = [...this.#tripsModel.points];
   }
 
   init() {
-    this.tripPoints = [...this.#tripsModel.points];
-
     this.#renderHeader();
+  }
+
+  get totalPrice() {
+    return this.tripPoints.reduce((totalPrice, {type, basePrice, offers}) => {
+      totalPrice += basePrice;
+
+      totalPrice += this.#tripsModel.getOffersById(type, offers).reduce((offersPrice, {price}) => {
+        offersPrice += price;
+        return offersPrice;
+      }, 0);
+
+      return totalPrice;
+    }, 0);
   }
 
   #renderHeader() {
     if (this.tripPoints.length > 0) {
-      render(new TripInfoView(), this.#container.querySelector('.trip-main'), RenderPosition.AFTERBEGIN);
+      render(new TripInfoView(this.totalPrice), this.#container.querySelector('.trip-main'), RenderPosition.AFTERBEGIN);
       render(new TripFiltersView(this.#filters), this.#container.querySelector('.trip-controls__filters'));
     }
   }
