fc95496270ce8baae2c44b2878ede5c6da5a8f09
Igor Khripunov <igor.khripunov@veeam.com>
2023-07-15 21:11:54 +0400

    Refactoring

--- src/utils/point.js
+++ src/utils/point.js
@@ -7,15 +7,15 @@ const isPointPresent = (point) => (dayjs().isAfter(point.dateFrom) && dayjs().is
 const isPointPast = (point) => dayjs().isAfter(point.dateTo);
 
 const getWeightForNullDate = (dateA, dateB) => {
-  if (dateA === null && dateB === null) {
+  if (!dateA && !dateB) {
     return 0;
   }
 
-  if (dateA === null) {
+  if (!dateA) {
     return 1;
   }
 
-  if (dateB === null) {
+  if (!dateB) {
     return -1;
   }
 
