21d38bcf51daadebb71c707c242b18554748496d
Igor Khripunov <igor.khripunov@veeam.com>
2023-07-31 14:29:32 +0400

    Add filter model

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import HeaderPresenter from './presenter/header-presenter.js';
 import MainPresenter from './presenter/main-presenter.js';
 import TripModel from './model/trip-model.js';
+import FilterModel from './model/filter-model.js';
 
 const $siteHeaderElement = document.querySelector('.page-header');
 const $siteMainElement = document.querySelector('.page-main');
 
 const tripsModel = new TripModel();
+const filterModel = new FilterModel();
+
 const headerPresenter = new HeaderPresenter({
   headerContainer: $siteHeaderElement,
   tripsModel
