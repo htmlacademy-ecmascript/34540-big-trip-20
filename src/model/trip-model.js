7baf24d27d698634b39718e47564a0dc2e8f7462
Igor Khripunov <igor.khripunov@veeam.com>
2023-08-25 13:34:27 +0400

    Add the Adapter pattern

--- src/model/trip-model.js
+++ src/model/trip-model.js
@@ -17,10 +17,7 @@ export default class TripModel extends Observable {
 
     this.#tripApiService.points.then((points) => {
       console.log(points);
-      // Есть проблема: cтруктура объекта похожа, но некоторые ключи называются иначе,
-      // а ещё на сервере используется snake_case, а у нас camelCase.
-      // Можно, конечно, переписать часть нашего клиентского приложения, но зачем?
-      // Есть вариант получше - паттерн "Адаптер"
+      console.log(points.map(this.#adaptToClient));
     });
   }
 
@@ -90,5 +87,21 @@ export default class TripModel extends Observable {
     ];
 
     this._notify(updateType);
+  }
+
+  #adaptToClient(point) {
+    const adaptedPoint = {...point,
+      dateFrom: point['date_from'] !== null ? new Date(point['date_from']) : point['date_from'],
+      dateTo: point['date_to'] !== null ? new Date(point['date_to']) : point['date_to'],
+      basePrice: point['base_price'],
+      isFavorite: point['is_favorite']
+    };
+
+    delete adaptedPoint['date_from'];
+    delete adaptedPoint['date_to'];
+    delete adaptedPoint['basePrice'];
+    delete adaptedPoint['is_favorite'];
+
+    return adaptedPoint;
   }
 }
--- src/service/trip-api-service.js
+++ src/service/trip-api-service.js
@@ -11,12 +11,28 @@ export default class TripApiService extends ApiService {
     const response = await this._load({
       url: `points/${point.id}`,
       method: ApiServiceMethod.PUT,
-      body: JSON.stringify(point),
+      body: JSON.stringify(this.#adaptToServer(point)),
       headers: new Headers({'Content-Type': 'application/json'}),
     });
 
     const parsedResponse = await ApiService.parseResponse(response);
 
     return parsedResponse;
+  }
+
+  #adaptToServer(point) {
+    const adaptedPoint = {...point,
+      'date_from': point.dateFrom instanceof Date ? point.dateFrom.toISOString() : null,
+      'date_to': point.dateTo instanceof Date ? point.dateTo.toISOString() : null,
+      'base_price': point.basePrice,
+      'is_favorite': point.isFavorite,
+    };
+
+    delete adaptedPoint.dateFrom;
+    delete adaptedPoint.date_to;
+    delete adaptedPoint.basePrice;
+    delete adaptedPoint.isFavorite;
+
+    return adaptedPoint;
   }
 }
